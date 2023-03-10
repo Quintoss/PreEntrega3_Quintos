abab1d765b775338930c850df3e847c98e603549
Emiliano Quintos <equintoss2@gmail.com>
2023-03-10 15:40:01 -0600

    Modifcaciones y estilos

---
+++ js/data.js
@@ -0,0 +1,72 @@
+const products = [
+  {
+    nombre: "Procesador AMD Ryzen 5 3600",
+    categoria: "Procesador",
+    precio: "5400",
+  },
+  {
+    nombre: "Procesador AMD Ryzen 7 5800X",
+    categoria: "Procesador",
+    precio: "7400",
+  },
+  {
+    nombre: "Procesador Intel Core i5-10400F",
+    categoria: "Procesador",
+    precio: "5200",
+  },
+  {
+    nombre: "Procesador Intel Core i5-10400F",
+    categoria: "Procesador",
+    precio: "5200",
+  },
+  {
+    nombre: "Procesador Intel Core i5-10400F",
+    categoria: "Procesador",
+    precio: "5200",
+  },
+  {
+    nombre: "Procesador Intel Core i5-10400F",
+    categoria: "Procesador",
+    precio: "5200",
+  },
+  {
+    nombre: "Procesador Intel Core i5-10400F",
+    categoria: "Procesador",
+    precio: "5200",
+  },
+  {
+    nombre: "Procesador Intel Core i5-10400F",
+    categoria: "Procesador",
+    precio: "5200",
+  },
+  {
+    nombre: "Procesador Intel Core i5-10400F",
+    categoria: "Procesador",
+    precio: "5200",
+  },
+  {
+    nombre: "Procesador Intel Core i5-10400F",
+    categoria: "Procesador",
+    precio: "5200",
+  },
+  {
+    nombre: "Procesador Intel Core i5-10400F",
+    categoria: "Procesador",
+    precio: "5200",
+  },
+  {
+    nombre: "Procesador Intel Core i5-10400F",
+    categoria: "Procesador",
+    precio: "5200",
+  },
+  {
+    nombre: "Procesador Intel Core i5-10400F",
+    categoria: "Procesador",
+    precio: "5200",
+  },
+  {
+    nombre: "Procesador Intel Core i5-10400F",
+    categoria: "Procesador",
+    precio: "5200",
+  },
+];
